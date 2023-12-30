5d15ee05921d0a15ca4a288aea0e018b4b6c4a3e
ebcoleman <140854597+ebcoleman@users.noreply.github.com>
2023-12-30 11:06:32 -0500

    the local storage now recalls when page is refreshed

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,11 @@
 
-  // calls the current day using day.js in the format I selected below
-  var currentDay =dayjs().format('dddd, MMMM D, YYYY');
-  // updates the content in the HTML 'current-day'
-  document.getElementById('current-day').textContent = currentDay;
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
 
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  // when the save button is clicked for each specific time block; that information is saved into the local storage to be recalled later
   $(document).on('click', '.saveBtn', function() {
     var timeBlockId = $(this).closest('.time-block').attr('id');
     console.log('Clicked Save for', timeBlockId);
@@ -28,15 +17,11 @@ $(function () {
     console.log('Stored in Local Storage:', localStorage.getItem(timeBlockId));
   });
   
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
 
   // calls the current hour using day.js
   var currentHour = dayjs().hour();
 
+  // applies past, present, or future to each time block to change to corresponding colors
   var timeblocks = document.querySelectorAll('.time-block');
   timeblocks.forEach(function(timeblock){
     var timeblockHour = parseInt(timeblock.id.split('-')[1]);
@@ -50,13 +35,24 @@ $(function () {
     }
   })
   
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+  
+// recalls the information that was saved into the local storage for each timeblock
+  timeblocks.forEach(function(timeblock) {
+    var timeBlockId = timeblock.id;
+    var userInput = localStorage.getItem(timeBlockId);
 
+    if (userInput) {
+      timeblock.querySelector('.description').value = userInput;
+    }
+  });
   
   // TODO: Add code to display the current date in the header of the page.
+
+    // calls the current day using day.js in the format I selected below
+    var currentDay =dayjs().format('dddd, MMMM D, YYYY');
+    // updates the content in the HTML 'current-day'
+    document.getElementById('current-day').textContent = currentDay;
+  
 
 
 });
--- README.md
+++ README.md
@@ -26,10 +26,10 @@ GIVEN I am using a daily planner to create a schedule
 <!-- THEN I am presented with timeblocks for standard business hours of 9am&ndash;5pm -->
 <!-- WHEN I view the timeblocks for that day
 THEN each timeblock is color coded to indicate whether it is in the past, present, or future -->
-WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
+<!-- WHEN I click into a timeblock
+THEN I can enter an event -->
+<!-- WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage -->
 WHEN I refresh the page
 THEN the saved events persist
 ```
